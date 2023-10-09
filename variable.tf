afdf4a4977066d103aba2b83c75d08814fb966a4
Syed Muhammad Danial <102377018+syed-danial@users.noreply.github.com>
2023-10-10 04:41:45 +0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,17 +1,25 @@
 variable "region" {
   type        = string
   description = "AWS Region"
+  default = "us-west-2"
 }
 
 variable "name" {
   type        = string
   description = "Author of code"
+  default = "danial"
 }
 
 
 variable "tags" {
   description = "Common tags for all resources"
   type        = map(map(string))
+  default = {
+    VPC = {
+      defuse = "2023-08-15"
+      cidr_block = "10.0.0.0/16"
+    }
+  }
 }
 
 variable "VPC" {
@@ -23,4 +31,11 @@ variable "VPC" {
     db_subnet_cidr_blocks  = list(string)
     create_nat_gateway     = bool
   })
+  default = {
+    vpc_cidr               = "10.0.0.0/16"
+    web_subnet_cidr_blocks = ["10.0.0.0/24", "10.0.1.0/24"]
+    app_subnet_cidr_blocks = ["10.0.2.0/24", "10.0.3.0/24"]
+    db_subnet_cidr_blocks  = ["10.0.4.0/24", "10.0.5.0/24"]
+    create_nat_gateway     = true
+  }
 }
